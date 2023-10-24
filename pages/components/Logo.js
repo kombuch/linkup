a40b02356b371626e788675089bd704e0d680b87
Daniel Koch <dan@dkoch.dev>
2023-10-24 18:35:09 -0500

    create logo

---
+++ pages/components/Logo.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Logo = () => {
+  return (
+    <View style={styles.logoBG}>
+      <Text style={styles.header}>Link Up</Text>
+    </View>
+  );
+};
+
+export default Logo;
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 40,
+    textAlign: "center",
+    color: "#fff",
+  },
+  logoBG: {
+    borderRadius: 20,
+    alignContent: "center",
+    justifyContent: "center",
+    width: 150,
+    height: 60,
+    backgroundColor: "#e87500",
+  },
+});
